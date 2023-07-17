3157679925a515a4cc3aff80d56877d50757027e
ProgBH <90292730+ProgBH@users.noreply.github.com>
2023-07-17 16:28:44 -0300

    agregado de llamada a backend en el front

--- js/indexJs.js
+++ js/indexJs.js
@@ -9,13 +9,47 @@ function sendForm() {
     document.getElementById("formLastName").value = ""
     document.getElementById("textArea").value = ""
 
-    Swal.fire({
-      icon: "success",
-      title: "Incripcion confirmada",
-      text: "Nos comunicaremos contigo a la brevedad.",
-      confirmButtonText: "Continuar",
-      confirmButtonColor: '#0297de',
-    });
+
+    const compraOrador = {};
+    compraOrador["nombre"] = name;
+    compraOrador["apellido"] = lastName;
+    compraOrador["tema"] = message;
+    compraOrador["fecha_creacion"]= new Date().getTime;
+
+    // Realiza la solicitud HTTP POST al backend
+    fetch("http://localhost:8080/api/oradores", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json"
+      
+      },
+      body: JSON.stringify(compraOrador)
+    })
+      .then(response => response.json())
+      .then(data => {
+
+        Swal.fire({
+          icon: "success",
+          title: "Incripcion confirmada",
+          text: "Nos comunicaremos contigo a la brevedad.",
+          confirmButtonText: "Continuar",
+          confirmButtonColor: '#0297de',
+        });
+
+      })
+      .catch(error => {
+
+        Swal.fire({
+          icon: "Error",
+          title: "Suucedio un error inesperado",
+          text: "Reintenta de nuevo ma tarde.",
+          confirmButtonText: "Cerrar",
+          confirmButtonColor: '#0297de',
+        });
+
+      });
+
+
   }
 
 
